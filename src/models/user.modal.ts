2fe1a9727469854e52bf42fc7e9b98e54ada7077
Ishan Jain <2021pietcsishan082@poornima.org>
2026-01-31 00:44:11 +0530

    user modal has been made

---
+++ src/models/user.modal.ts
@@ -0,0 +1,52 @@
+import { model, Model, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    twoFactorEnabled: {
+      type: Boolean,
+      default: false,
+    },
+    twoFactorSecret: {
+      type: Boolean,
+      required: true,
+    },
+    tokenVersion: {
+      type: Number,
+      default: 0,
+    },
+    resetPasswordToken: {
+      type: String,
+      default: undefined,
+    },
+    resetPaswordExpires: {
+      type: Date,
+      default: undefined,
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = model("User", userSchema);
